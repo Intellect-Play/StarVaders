633d0746ca84e474a3cfa52ea3909fe44afb4215
muradmikayilzadeh <muradm@uoregon.edu>
2025-07-04 13:47:42 +0400

    SDK integration

---
+++ Assets/Scripts/GameManagerMain.cs
@@ -0,0 +1,43 @@
+
+using GameAnalyticsSDK;
+using UnityEngine;
+
+public class GameManagerMain : MonoBehaviour
+{
+    public static GameManagerMain Instance { get; private set; }
+
+    [SerializeField]private GameObject mMainStage;
+    [SerializeField]private GameObject mGameUI;
+    [SerializeField] private GameObject mGameShop;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        GameAnalytics.Initialize();
+        BattleStartInMainMenu(false);
+    }
+
+    public void BattleStartInMainMenu(bool start)
+    {
+        mMainStage.SetActive(!start);
+        mGameShop.SetActive(false);
+        mGameUI.SetActive(start);
+    }
+    public void OpenShop()
+    {
+        mGameShop.SetActive(true);
+        mMainStage.SetActive(false);
+    }
+    public void CloseShop()
+    {
+        mGameShop.SetActive(false);
+        mMainStage.SetActive(true);
+    }
+}
+
